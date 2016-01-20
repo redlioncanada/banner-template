9f0825c66d0eee0cfdbb9c24330a68534deb8b99
Stephen Poole <stephen.poole@redlioncanada.com>
2016-01-20 16:38:47 -0500

    Completed general placement and animations of 160x600 and 728x90.

--- app/templates/js/animations/728x90.js
+++ app/templates/js/animations/728x90.js
@@ -1,11 +1,17 @@
-var timeline = new TimelineLite({onComplete: onComplete, onStart: function(){console.log('started skyscraper timeline')}});
+var easeIn = Power1.easeIn;
+var easeOut = Power1.easeOut;
+var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
-            .to('#copy1', 1, {opacity:1});
+            .to('#copy1', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(2, 'frame2');
         timeline.add('frame2')
-            .to('#copy1', 1, {opacity:0})
-            .to('#copy2', 1, {opacity:1});
+            .to('#copy1', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy2', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(1.5, 'frame3');
         timeline.add('frame3')
-            .to('#copy2', 1, {opacity:0})
-            .to('#background2', 1, {opacity:1})
-            .to('#copy3, #cta', 1, {opacity:1});
+            .to('#gradient', 0.8, {left:-210, ease:easeIn}, 'frame3')
+            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame3')
+            .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame3')
+            .to('#background2', 0.8, {opacity:1, ease:easeIn})
+            .to('#copy3, #cta', 0.8, {opacity:1, ease:easeIn});
         timeline.add('frame4').add('end');
