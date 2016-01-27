a5830413417c0edd4037717f58cb7762ae5b1c38
Stephen Poole <stephen.poole@redlioncanada.com>
2016-01-27 10:53:31 -0500

    Update copy and assets.

--- app/templates/js/animations/160x600.js
+++ app/templates/js/animations/160x600.js
@@ -5,15 +5,11 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
             .addDelay(2, 'frame2');
         timeline.add('frame2')
-            .to('#copy1', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy2', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'frame3');
-        timeline.add('frame3')
-            .to('#gradient', 0.8, {bottom:"-=134", ease:easeIn}, 'frame3')
-            .to('#gradient1', 0.8, {height: 134, ease:easeIn}, 'frame3')
-            .to('#background3', 0.8, {bottom: "-=134", ease:easeIn}, 'frame3')
-            .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame3')
+            .to('#gradient', 0.8, {bottom:"-=134", ease:easeIn}, 'frame2')
+            .to('#gradient1', 0.8, {height: 134, ease:easeIn}, 'frame2')
+            .to('#background3', 0.8, {bottom: "-=134", ease:easeIn}, 'frame2')
+            .to('#copy1', 0.8, {opacity:0, ease:easeOut}, 'frame2')
             .to('#background1,#background3,#gradient,#gradient1', 0, {opacity:0})
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
-            .to('#copy3, #cta', 0.8, {opacity:1, ease:easeIn});
-        timeline.add('frame4').add('end');
+            .to('#copy2, #cta', 0.8, {opacity:1, ease:easeIn});
+        timeline.add('frame3').add('end');
--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -7,15 +7,11 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame4');
+            .addDelay(1.5, 'frame4');
         timeline.add('frame4')
-            .to('#copy1', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy2', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'frame5');
-        timeline.add('frame5')
-            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame5')
-            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame5')
-            .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame5')
+            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame4')
+            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame4')
+            .to('#copy1', 0.8, {opacity:0, ease:easeOut}, 'frame4')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
-            .to('#copy3, #cta', 0.8, {opacity:1, ease:easeIn});
-        timeline.add('frame6').add('end');
+            .to('#copy2, #cta', 0.8, {opacity:1, ease:easeIn});
+        timeline.add('frame5').add('end');
