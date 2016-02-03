fb9e68aab6796668e0a3ad4322c34aaa51b3641e
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-03 17:30:35 -0500

    Almost finished 300x250. Finished rough of animation.

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,19 +3,22 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:0})
-            .addDelay(1.5, 'frame3');
-        timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame4');
-        timeline.add('frame4')
+            .addDelay(2, 'frame3');
+        timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(1.5, 'frame4');
+        timeline.add('frame4')
+            .to('#copy2,#background1', 0.8, {opacity:0, ease:easeOut},'frame4')
+            .to('#copy3,#background2', 0.8, {opacity:1, ease:easeIn},'frame4')
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
-            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame5')
-            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame5')
-            .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame5')
-            .to('#background2', 0.8, {opacity:1, ease:easeIn})
-            .to('#copy3, #cta', 0.8, {opacity:1, ease:easeIn});
-        timeline.add('frame6').add('end');
+            .to('#copy3', 0.8, {opacity:0, ease:easeOut})
+            .to('#everydaycare', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(1.5, 'frame6');
+        timeline.add('frame6')
+            .to('#everydaycare,#background2', 0.8, {opacity:0, ease:easeOut}, 'frame6')
+            .to('#copy4,#cta,#background3', 0.8, {opacity:1, ease:easeIn}, 'frame6')
+            .addDelay(1.5, 'frame6');
+        timeline.add('frame7').add('end');
