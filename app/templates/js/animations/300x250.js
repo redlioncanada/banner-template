15d179543d9ac134709348c91b1e926063b30254
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 10:00:45 -0500

    Separated copy4 into it's own html include to allow for <br/> insertion on one banner size

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -4,18 +4,16 @@ var speed = 1.5;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .fromTo('#copy1', speed, {left:'+=100'}, {left:'-=100'}, 'frame2')
-            .to('#copy1', speed, {opacity: 1, ease:easeIn}, 'frame2')
+            .fromTo('#copy1 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}, {css:{marginLeft: '+=265'}}, 'frame2')
             .addDelay(2, 'frame3');
         timeline.add('frame3')
-            .to('#copy1', speed, {opacity:0, ease:easeOut}, 'frame3')
-            .fromTo('#copy2', speed, {left:'+=100'}, {left:'-=100'}, 'frame3')
-            .to('#copy2', speed, {opacity:1, ease:easeIn}, 'frame3')
+            .to('#copy1 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}).add('frame3-copy1')
+            .fromTo('#copy2 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}, {css:{marginLeft: '+=265'}}, 'frame3-copy1')
+            .fromTo('#copy2', speed, {opacity: 1})
             .addDelay(2.5, 'frame4');
         timeline.add('frame4')
             .to('#background1,#copy2', speed, {opacity:0, ease:easeOut},'frame4')
             .to('#copy3,#background2', speed, {opacity:1, ease:easeIn},'frame4')
-            .fromTo('#copy3', speed, {left:'+=100'}, {left:'-=100'}, 'frame4')
             .addDelay(3, 'frame5');
         timeline.add('frame5')
             .to('#copy3,#yellowbar', speed, {opacity:0, ease:easeOut})
@@ -26,3 +24,5 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('#copy4,#cta,#background3', speed, {opacity:1, ease:easeIn}, 'frame6')
             .addDelay(2.5, 'frame6');
         timeline.add('frame7').add('end');
+
+timeline.seek('frame4').pause()
