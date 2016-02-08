2cb37b37033e3e37f20db84f06c14e6e80893257
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 11:27:40 -0500

    Added 0.5s to frame2 on each banner. Adjusted gradient positioning for 300x250.

--- app/templates/js/animations/160x600.js
+++ app/templates/js/animations/160x600.js
@@ -7,7 +7,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame2')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'frame3');
+            .addDelay(2.5, 'frame3');
         timeline.add('frame3')
             .to('#gradient', 0.8, {bottom:"-=134", ease:easeIn}, 'frame3')
             .to('#gradient1', 0.8, {height: 134, ease:easeIn}, 'frame3')
--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,17 +3,17 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:0})
+            .to('#gradient', 1, {left:40})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame4');
+            .addDelay(2.5, 'frame4');
         timeline.add('frame4')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
-            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame5')
+            .to('#gradient', 0.8, {left:-({width} - 40), ease:easeIn}, 'frame5')
             .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame5')
             .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame5')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
