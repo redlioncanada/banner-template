b10f474162eda5f94761bacdcd75604818d07e57
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 13:24:04 -0500

    Adjusted animation as per feedback

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -8,21 +8,23 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .addDelay(2, 'frame3');
         timeline.add('frame3')
             .to('#copy1 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}).add('frame3-copy1')
+            .to('#copy2', 0, {opacity:1}, 'frame3-copy1')
             .fromTo('#copy2 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}, {css:{marginLeft: '+=265'}}, 'frame3-copy1')
-            .fromTo('#copy2', speed, {opacity: 1})
             .addDelay(2.5, 'frame4');
         timeline.add('frame4')
-            .to('#background1,#copy2', speed, {opacity:0, ease:easeOut},'frame4')
-            .to('#copy3,#background2', speed, {opacity:1, ease:easeIn},'frame4')
+            .to('#copy2 .copy-wrapper', speed, {css:{marginLeft: '-=265'}},'frame4')
+            .to('#background1', speed, {opacity:0, ease:easeOut},'frame4')
+            .to('#background2', speed, {opacity:1, ease:easeIn},'frame4').add('frame4-copy3')
+            .to('#copy3', 0, {opacity:1}, 'frame4-copy3')
+            .fromTo('#copy3 .copy-wrapper', speed, {css:{marginLeft: '-=265'}}, {css:{marginLeft: '+=265'}}, 'frame4-copy3')
             .addDelay(3, 'frame5');
         timeline.add('frame5')
-            .to('#copy3,#yellowbar', speed, {opacity:0, ease:easeOut})
-            .to('#everydaycare', speed, {opacity:1, ease:easeIn})
+            .to('#copy3 .copy-wrapper', speed, {css:{marginLeft: '-=265'}},'frame5')
+            .to('#yellowbar', speed, {opacity:0, ease:easeOut}).add('frame5-yellowbar')
+            .to('#everydaycare', speed, {opacity:1, ease:easeIn},'frame5-yellowbar')
             .addDelay(2.5, 'frame6');
         timeline.add('frame6')
             .to('#everydaycare,#background2', speed, {opacity:0, ease:easeOut}, 'frame6')
             .to('#copy4,#cta,#background3', speed, {opacity:1, ease:easeIn}, 'frame6')
             .addDelay(2.5, 'frame6');
         timeline.add('frame7').add('end');
-
-timeline.seek('frame4').pause()
