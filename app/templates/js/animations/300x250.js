d4e6237f79e1d15000506a490decbc8cb8ff2ab9
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 11:21:43 -0500

    Repositioned gradient on 300x250

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,7 +3,7 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:0})
+            .to('#gradient', 1, {left:70})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
@@ -13,7 +13,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
-            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame5')
+            .to('#gradient', 0.8, {left:-({width} - 70), ease:easeIn}, 'frame5')
             .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame5')
             .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame5')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
