400cf91f0da026fc6265c8b0886160d09c18134a
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 11:13:56 -0500

    Moved copy1 down on 160x600. Adjusted gradient positioning on all sizes.

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,13 +3,13 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:0})
+            .to('#gradient', 1, {left:30})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame4');
         timeline.add('frame4')
-            .to('#gradient', 0.8, {left:-{width}, ease:easeIn}, 'frame4')
+            .to('#gradient', 0.8, {left:-({width}-30), ease:easeIn}, 'frame4')
             .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame4')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut}, 'frame4')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
--- app/templates/js/animations/728x90.js
+++ app/templates/js/animations/728x90.js
@@ -5,7 +5,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('#copy1', 0.8, {opacity:1, ease:easeIn})
             .addDelay(2, 'frame2');
         timeline.add('frame2')
-            .to('#gradient', 0.8, {left:-165, ease:easeIn}, 'frame2')
+            .to('#gradient', 0.8, {left:-165+90, ease:easeIn}, 'frame2')
             .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame2')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut}, 'frame2')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
