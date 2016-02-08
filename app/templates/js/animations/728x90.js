a98354f8cc348d7a7f911b128cddd8d346d11922
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-08 11:05:01 -0500

    Moved copy1 on 160x600 down, adjusted gradient to allow user to see underlying image more on all sizes.

--- app/templates/js/animations/728x90.js
+++ app/templates/js/animations/728x90.js
@@ -7,9 +7,9 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame2')
             .to('#copy1', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'frame3');
+            .addDelay(2, 'frame3');
         timeline.add('frame3')
-            .to('#gradient', 0.8, {left:-165, ease:easeIn}, 'frame3')
+            .to('#gradient', 0.8, {left:-165+90, ease:easeIn}, 'frame3')
             .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame3')
             .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame3')
             .to('#background2', 0.8, {opacity:1, ease:easeIn})
