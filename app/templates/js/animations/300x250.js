367225b5df449583616dae018b454ccc3e77c8e7
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-09 09:47:55 -0500

    Break up copy1 into 3 separate pieces of copy.

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -5,11 +5,19 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame2')
             .to('#gradient', 1, {left:70})
             .addDelay(1.5, 'frame3');
-        timeline.add('frame3')
-            .to('#copy1', 0.8, {opacity:1, ease:easeIn})
+        timeline.add('frame3-prefix').add('start')
+            .to('#copy1-prefix', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(2, 'frame3-highlight');
+        timeline.add('frame3-highlight')
+            .to('#copy1-prefix', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy1-highlight', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(2, 'frame3-suffix');
+        timeline.add('frame3-suffix')
+            .to('#copy1-highlight', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy1-suffix', 0.8, {opacity:1, ease:easeIn})
             .addDelay(2, 'frame4');
         timeline.add('frame4')
-            .to('#copy1', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy1-suffix', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
