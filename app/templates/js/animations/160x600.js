7a467d956c2f4b200f0e8e1190839e2144bdef2c
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-09 10:23:07 -0500

    Changed copy1 to be 1 frame.

--- app/templates/js/animations/160x600.js
+++ app/templates/js/animations/160x600.js
@@ -1,19 +1,15 @@
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
-        timeline.add('frame1-prefix').add('start')
+        timeline.add('frame1').add('start')
             .to('#copy1-prefix', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame1-highlight');
-        timeline.add('frame1-highlight')
-            .to('#copy1-prefix', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy1-highlight', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame1-suffix');
-        timeline.add('frame1-suffix')
-            .to('#copy1-highlight', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy1-suffix', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(1.5, 'frame1-highlight')
+            .to('#copy1-highlight', 0.8, {opacity:1, ease:easeIn}).add('frame1-highlight')
+            .addDelay(1.5, 'frame1-suffix')
+            .to('#copy1-suffix', 0.8, {opacity:1, ease:easeIn}).add('frame1-suffix')
             .addDelay(2, 'frame2');
         timeline.add('frame2')
-            .to('#copy1-suffix', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy1-prefix,#copy1-highlight,#copy1-suffix', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -5,19 +5,15 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame2')
             .to('#gradient', 1, {left:70})
             .addDelay(1.5, 'frame3');
-        timeline.add('frame3-prefix').add('start')
+        timeline.add('frame3').add('start')
             .to('#copy1-prefix', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame3-highlight');
-        timeline.add('frame3-highlight')
-            .to('#copy1-prefix', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy1-highlight', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame3-suffix');
-        timeline.add('frame3-suffix')
-            .to('#copy1-highlight', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy1-suffix', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(1.5, 'frame3-highlight')
+            .to('#copy1-highlight', 0.8, {opacity:1, ease:easeIn}).add('frame3-highlight')
+            .addDelay(1.5, 'frame3-suffix')
+            .to('#copy1-suffix', 0.8, {opacity:1, ease:easeIn}).add('frame3-suffix')
             .addDelay(2, 'frame4');
         timeline.add('frame4')
-            .to('#copy1-suffix', 0.8, {opacity:0, ease:easeOut})
+            .to('#copy1-prefix,#copy1-highlight,#copy1-suffix', 0.8, {opacity:0, ease:easeOut})
             .to('#copy2', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
