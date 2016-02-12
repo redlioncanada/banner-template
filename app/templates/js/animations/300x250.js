937d5096f3f9dd586a862b42aa69eead45f92090
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-12 15:48:51 -0500

    300x250 - Moved gradient to the left so the copy is easier to see.

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,7 +3,7 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:70})
+            .to('#gradient', 1, {left:50})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3').add('start')
             .to('#copy1-prefix', 0.8, {opacity:1, ease:easeIn})
