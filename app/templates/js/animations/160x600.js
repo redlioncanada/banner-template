c4be5a695a058469b4532ef782216b523d5d9bd4
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-16 10:42:43 -0500

    Changed order of text animation on frames 1 & 2

--- app/templates/js/animations/160x600.js
+++ app/templates/js/animations/160x600.js
@@ -2,15 +2,13 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
-            .to('#copy1 .highlight', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'copy1-prefix')
-            .to('#copy1 span, #copy1-prefix span', 0.8, {opacity:1, ease:easeIn}).add('copy1-prefix')
+            .to('#copy1 span, #copy1-prefix span, #copy1 .highlight', 0.8, {opacity:1, ease:easeIn}).add('copy1-prefix')
             .addDelay(2, 'frame2');
         timeline.add('frame2')
             .to('#copy1,#copy1-prefix', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy2 .highlight', 0.8, {opacity:1, ease:easeIn})
+            .to('#copy2-prefix span', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'copy2-prefix')
-            .to('#copy2 span, #copy2-prefix span', 0.8, {opacity:1, ease:easeIn}).add('copy2-prefix')
+            .to('#copy2 span, #copy2-prefix span, #copy2 .highlight', 0.8, {opacity:1, ease:easeIn}).add('copy2-prefix')
             .addDelay(2.5, 'frame3');
         timeline.add('frame3')
             .to('#gradient', 0.8, {bottom:"-=134", ease:easeIn}, 'frame3')
--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -6,15 +6,13 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('#gradient', 1, {left:20})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
-            .to('#copy1 .highlight', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'copy1-prefix')
-            .to('#copy1 span, #copy1-prefix span', 0.8, {opacity:1, ease:easeIn}).add('copy1-prefix')
+            .to('#copy1 span, #copy1-prefix span, #copy1 .highlight', 0.8, {opacity:1, ease:easeIn}).add('copy1-prefix')
             .addDelay(2.5, 'frame4');
         timeline.add('frame4')
             .to('#copy1,#copy1-prefix', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy2 .highlight', 0.8, {opacity:1, ease:easeIn})
+            .to('#copy2-prefix span', 0.8, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'copy2-prefix')
-            .to('#copy2 span, #copy2-prefix span', 0.8, {opacity:1, ease:easeIn}).add('copy2-prefix')
+            .to('#copy2 span, #copy2-prefix span, #copy2 .highlight', 0.8, {opacity:1, ease:easeIn}).add('copy2-prefix')
             .addDelay(1.5, 'frame5');
         timeline.add('frame5')
             .to('#gradient', 0.8, {left:-({width} - 40), ease:easeIn}, 'frame5')
