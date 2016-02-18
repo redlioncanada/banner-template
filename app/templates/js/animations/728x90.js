834e8c0300fc05e2c364bc3c59a32db5248f3f74
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-18 14:05:55 -0500

    Added assets for every size. Completed animation for 728x90.

--- app/templates/js/animations/728x90.js
+++ app/templates/js/animations/728x90.js
@@ -1,17 +1,91 @@
-var easeIn = Power1.easeIn;
-var easeOut = Power1.easeOut;
-var timeline = new TimelineLite({onComplete: onComplete});
-        timeline.add('frame1').add('start')
-            .to('#copy2 .prefix', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(2, 'frame2');
-        timeline.add('frame2')
-            .to('#copy2 .prefix', 0.8, {opacity:0, ease:easeOut})
-            .to('#copy2 .suffix', 0.8, {opacity:1, ease:easeIn})
-            .addDelay(1.5, 'frame3');
-        timeline.add('frame3')
-            .to('#gradient', 0.8, {left:-165, ease:easeIn}, 'frame3')
-            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame3')
-            .to('#copy2', 0.8, {opacity:0, ease:easeOut}, 'frame3')
-            .to('#background2', 0.8, {opacity:1, ease:easeIn})
-            .to('#copy3, #cta', 0.8, {opacity:1, ease:easeIn});
-        timeline.add('frame4').add('end');
+$(function() {
+    var back01 = $("#back01");
+    var back02 = $("#back02");
+    var back03 = $("#back03");
+
+    var edcLogo = $("#edc-logo");
+    var wpLogo = $("#wp-logo");
+    var cta = $("#cta");
+    var ctaOver = $("#cta > .ctaDown");
+
+    var copyContainer = $("#copy-container");
+    var copyDiv = $("#copy-holder");
+    var copyDisplay = $("#copy-holder > p");
+    var containerHeight = 65;
+    var copyContainerWidth = 400;
+
+    var yellowBar = $("#yellow-bar");
+    var copyNum = 0;
+    var delayCount = 1;
+
+    cta.mouseenter(function(){
+        TweenMax.to(ctaOver,0.5, {opacity:1, ease:Power1.easeOut})
+    });
+
+    cta.mouseout(function(){
+        TweenMax.to(ctaOver,0.5, {opacity:0, ease:Power1.easeOut})
+    });
+
+
+    function updateCopy(){
+        console.log(copyDiv.height())
+        copyDisplay.html(mainBannerCopy[copyNum]);
+        var newTop = (containerHeight - copyDiv.height())/2;
+        copyDiv.css('top', newTop);
+    }
+
+    function animateBack(){
+        updateCopy();
+        TweenMax.to(copyContainer,.8, {left:15, width:copyContainerWidth, ease:Power1.easeInOut, delay:.2})
+        TweenMax.to(yellowBar,.8, {left:15, ease:Power1.easeInOut, delay:.2, onComplete:playText})
+    }
+
+    function playText(){
+        console.log('playtext');
+        if((copyNum +1) < mainBannerCopy.length){
+            console.log('playtext '+copyNum);
+            var aniDelay = delayCount * 1.5;
+            // Animation Block 01
+            TweenMax.to(copyContainer,.6, {left:copyContainerWidth, width:1, delay:aniDelay});
+            TweenMax.to(yellowBar,.6, {left:copyContainerWidth, ease:Power1.easeOut, delay:aniDelay, onComplete:animateBack});
+            copyNum++;
+            delayCount ++;
+        } else {
+            console.log('playtext end');
+            playEndFrame();
+        }
+    }
+
+    function showBack(){
+        updateCopy();
+        TweenMax.to(back01, 1, {opacity:1, ease:Power1.easeInOut});
+        TweenMax.to(wpLogo, 1, {opacity:1, ease:Power1.easeInOut, delay:.5});
+        TweenMax.to(copyContainer, 1, {opacity:1, ease:Power1.easeInOut});
+        TweenMax.to(yellowBar,.6, {opacity:1, ease:Power1.easeIn, delay:.5, onComplete:playText});
+        TweenMax.to(back02, 1, {opacity:1, ease:Power1.easeIn, delay:9});
+    }
+
+    function showEndCopy(){
+        console.log('showend');
+        TweenMax.to(edcLogo, 1, {opacity:0, ease:Power1.easeIn, delay:.5});
+        copyDisplay.html(endFrameCopy);
+        copyDiv.css('top', '9px');
+        copyDiv.css('right', '-3px');
+        copyDisplay.css('width', '170px');
+        copyDisplay.css('font-size', '14px');
+        copyDisplay.css('font-weight', '400');
+        TweenMax.to(copyContainer, .8, {opacity:1, ease:Power1.easeOut, delay:1});
+        TweenMax.to(cta, .8, {opacity:1, ease:Power1.easeOut, delay:1.5});
+    }
+
+    function playEndFrame(){
+        console.log('playendframe');
+        TweenMax.to(yellowBar,.6, {opacity:0, ease:Power1.easeIn, delay:3.5});
+        TweenMax.to(copyContainer,.6, {opacity:0, ease:Power1.easeIn, delay:3});
+        //
+        TweenMax.to(edcLogo, 1, {opacity:1, ease:Power1.easeIn, delay:4});
+        //
+        TweenMax.to(back03, 1, {opacity:1, ease:Power1.easeIn, delay:6, onComplete:showEndCopy});
+    }
+    showBack();
+});
--- app/templates/js/en/copy.js
+++ app/templates/js/en/copy.js
@@ -1,2 +1,2 @@
-var mainBannerCopy = ["Care","Care is orgasnized", "Our Perfect Places&trade; system delivers 9 dedicated storage zones"];
+var mainBannerCopy = ["Care","Care is organized", "Our Perfect Places&trade; system delivers 9 dedicated storage zones"];
 var endFrameCopy = "See all of the innovation inside the Double Drawer Refrigerator";
