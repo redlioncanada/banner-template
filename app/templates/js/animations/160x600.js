65a770d50738addf4500c1792ff87ce72a1ad588
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-18 14:42:05 -0500

    Completed animation for 160x600

--- app/templates/js/animations/160x600.js
+++ app/templates/js/animations/160x600.js
@@ -1,4 +1,5 @@
 $(function() {
+    mainBannerCopy[1] = "Care is<br/>organized";
     var back01 = $("#back01");
     var back02 = $("#back02");
     var back03 = $("#back03");
@@ -11,7 +12,9 @@ $(function() {
     var copyContainer = $("#copy-container");
     var copyDiv = $("#copy-holder");
     var copyDisplay = $("#copy-holder > p");
-    var containerHeight = 65;
+    var containerHeight = 95;
+    var copyContainerWidth = 144;
+    var copyContainerLeft = 10;
 
     var yellowBar = $("#yellow-bar");
     var copyNum = 0;
@@ -29,14 +32,15 @@ $(function() {
     function updateCopy(){
         console.log(copyDiv.height())
         copyDisplay.html(mainBannerCopy[copyNum]);
+        console.log(containerHeight, copyDiv.height());
         var newTop = (containerHeight - copyDiv.height())/2;
         copyDiv.css('top', newTop);
     }
 
     function animateBack(){
         updateCopy();
-        TweenMax.to(copyContainer,.8, {left:15, width:250, ease:Power1.easeInOut, delay:.2})
-        TweenMax.to(yellowBar,.8, {left:15, ease:Power1.easeInOut, delay:.2, onComplete:playText})
+        TweenMax.to(copyContainer,.8, {left:copyContainerLeft, width:copyContainerWidth, ease:Power1.easeInOut, delay:.2})
+        TweenMax.to(yellowBar,.8, {left:copyContainerLeft, ease:Power1.easeInOut, delay:.2, onComplete:playText})
     }
 
     function playText(){
@@ -45,9 +49,8 @@ $(function() {
             console.log('playtext '+copyNum);
             var aniDelay = delayCount * 1.5;
             // Animation Block 01
-            TweenMax.to(copyContainer,.6, {left:250, width:1, delay:aniDelay});
-            TweenMax.to(yellowBar,.6, {left:250, ease:Power1.easeOut, delay:aniDelay, onComplete:animateBack});
-
+            TweenMax.to(copyContainer,.6, {left:copyContainerWidth, width:1, delay:aniDelay});
+            TweenMax.to(yellowBar,.6, {left:copyContainerWidth, ease:Power1.easeOut, delay:aniDelay, onComplete:animateBack});
             copyNum++;
             delayCount ++;
         } else {
@@ -68,12 +71,11 @@ $(function() {
     function showEndCopy(){
         console.log('showend');
         TweenMax.to(edcLogo, 1, {opacity:0, ease:Power1.easeIn, delay:.5});
+        copyContainer.css('top', 479);
         copyDisplay.html(endFrameCopy);
-        copyDiv.css('top', '21px');
-        copyDiv.css('right', '-3px');
-        copyDisplay.css('width', '150px');
-        copyDisplay.css('font-size', '12px');
-        copyDisplay.css('font-weight', '400');
+        copyDiv.css({'top': 11, 'right': 2, 'width': 144});
+        copyDisplay.addClass('centered');
+        copyDisplay.css({'font-size': 12, 'width': 170});
         TweenMax.to(copyContainer, .8, {opacity:1, ease:Power1.easeOut, delay:1});
         TweenMax.to(cta, .8, {opacity:1, ease:Power1.easeOut, delay:1.5});
     }
@@ -82,9 +84,7 @@ $(function() {
         console.log('playendframe');
         TweenMax.to(yellowBar,.6, {opacity:0, ease:Power1.easeIn, delay:3.5});
         TweenMax.to(copyContainer,.6, {opacity:0, ease:Power1.easeIn, delay:3});
-        //
         TweenMax.to(edcLogo, 1, {opacity:1, ease:Power1.easeIn, delay:4});
-        //
         TweenMax.to(back03, 1, {opacity:1, ease:Power1.easeIn, delay:6, onComplete:showEndCopy});
     }
     showBack();
