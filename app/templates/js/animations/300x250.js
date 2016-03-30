ac22bed17b0efe47ddd2ced65a3e503afcf81970
Stephen Poole <stephen.poole@redlioncanada.com>
2016-03-30 16:11:25 -0400

    Completed initial animation for 300x250

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -3,15 +3,13 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
         timeline.add('frame2')
-            .to('#gradient', 1, {left:30})
+            .to('#copy1,#subtitle,#man,#legal', 0.5, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame3');
         timeline.add('frame3')
-            .to('#copy1', 0.8, {opacity:1, ease:easeIn})
+            .to('#copy1,#subtitle,#legal', 0.5, {opacity:0, ease:easeOut})
+            .to('#copy2', 0.5, {opacity:1, ease:easeIn})
             .addDelay(1.5, 'frame4');
         timeline.add('frame4')
-            .to('#gradient', 0.8, {left:-({width}-30), ease:easeIn}, 'frame4')
-            .to('#background3', 0.8, {left:0, ease:easeIn}, 'frame4')
-            .to('#copy1', 0.8, {opacity:0, ease:easeOut}, 'frame4')
-            .to('#background2', 0.8, {opacity:1, ease:easeIn})
-            .to('#copy2, #cta', 0.8, {opacity:1, ease:easeIn});
+            .to('#copy2,#man', 0.5, {opacity:0, ease:easeOut})
+            .to('#copy3,#washer,#button,#logo', 0.5, {opacity:1, ease:easeIn});
         timeline.add('frame5').add('end');
