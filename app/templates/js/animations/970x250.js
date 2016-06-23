84971f408972850b35782950a0b9be3ba7f670a1
Stephen Poole <stephen.poole@redlioncanada.com>
2016-06-23 16:22:39 -0400

    Completed initial build according to PSD

--- app/templates/js/animations/970x250.js
+++ app/templates/js/animations/970x250.js
@@ -3,18 +3,18 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
             .to('.copy1,.logo', 2, {opacity:1, ease:easeIn}, 'start')
-            .addDelay(1, 'frame2')
+            .addDelay(2, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 1.5, {opacity: 0, ease: easeOut}, 'frame2')
-            .to('.copy2,.cta', 1.5, {opacity:1, ease:easeIn}, 'frame2')
-        timeline.add('frame3').add('end');
+            .to('.copy2', 1.5, {opacity:1, ease:easeIn}, 'frame2')
+            .addDelay(2, 'frame3')
+        timeline.add('frame3')
+            .to('.cta', 1.5, {opacity:1, ease:easeIn}, 'frame3')
+        timeline.add('frame4').add('end');
 
 
         timeline.add('masks')
-            .to('.masks .top', 2, {width:0,left:"+=70"}, 'start')
-            .to('.masks .left', 2, {height:0,top:"+=60"}, 'start')
-            .to('.masks .bottom-right', 2, {width:0,left:"+=100"}, 'start')
-            .to('.masks .bottom-bottom', 2, {height:0,top:"+=50"}, 'start')
-        timeline.add('frame3Background')
-            .to('.background1', 1.5, {opacity:0, ease:easeIn}, 'frame2')
-            .to('.background2', 1.5, {opacity:1, ease:easeIn}, 'frame2')
+            .to('.masks .top', 2, {width:0,left:"+=50"}, 'start')
+            .to('.masks .left', 2, {height:0,top:"+=50"}, 'start')
+            .to('.masks .bottom-right', 2, {width:0,left:"+=240"}, 'start')
+            .to('.masks .bottom-bottom', 2, {height:0,top:"+=56"}, 'start')
