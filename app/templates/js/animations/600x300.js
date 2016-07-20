1581edd5fa2d6b3c75d23a226430fea0bdeeabe3
Stephen Poole <stephen.poole@redlioncanada.com>
2016-07-20 11:15:10 -0400

    Completed animation

--- app/templates/js/animations/600x300.js
+++ app/templates/js/animations/600x300.js
@@ -2,14 +2,13 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1').add('start')
-            .to('.background1', 4, {css:{transform: 'matrix(1.2, 0, 0, 1.2, -30, 0)'}})
-            .to('.background1', 1, {opacity: 0, ease:easeOut}, 2)
-            .addDelay(2, 'frame2')
+            .to('.background1', 9, {css:{transform: 'matrix(1.3, 0, 0, 1.3, -90, 0)'}})
+            .to('.background1', 1, {opacity: 0, ease:easeOut}, 8)
         timeline.add('frame2')
-            .to('.copy1', 0.8, {opacity: 0, ease: easeOut})
-            .addDelay(0.5, 'frame2logo')
-            .to('.logo', 0.8, {opacity:1, ease:easeIn}).add('frame2logo')
-            .addDelay(1, 'frame3')
+            .addDelay(1.4, 'frame2copy')
+            .to('.copy1', 0.8, {opacity: 0, ease: easeOut}).add('frame2copy')
+            .to('.logo', 0.8, {opacity:1, ease:easeIn})
+            .addDelay(0.7, 'frame3')
         timeline.add('frame3')
-            .to('.copy2,.cta', 1.5, {opacity:1, ease:easeIn})
+            .to('.copy2,.cta', 1, {opacity:1, ease:easeIn})
         timeline.add('frame4').add('end');
