98d91f20f508b76ebda2c2f4bc5588544624c924
Storm Henry <storm.henry@redlioncanada.com>
2016-08-03 11:10:24 -0400

    Updated animations and added final frame

--- app/templates/js/animations/300x250.js
+++ app/templates/js/animations/300x250.js
@@ -2,12 +2,13 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
        timeline.add('frame1').add('start')
-          .to('.bg1,.stars', 1, {opacity: 1, ease: easeIn})
-          .to('.test', 1, {opacity:1, ease:easeIn})
-          .to('.legal', 1, {opacity:1, ease:easeIn})
+        .to('.bg1,.stars', 1.5, {opacity: 1, ease: easeIn})
+          .to('.test', 1.5, {opacity:1, ease:easeIn})
+          .to('.legal', 1.5, {opacity:1, ease:easeIn})
+          .addDelay(2, 'frame2')
           timeline.add('frame2')
-          .to('.backgroundfade', 1, {opacity:0, ease:easeOut});
-//             .addDelay(0.7, 'frame3')
-//         timeline.add('frame3')
-//             .to('.copy2,.cta', 1, {opacity:1, ease:easeIn})
-//         timeline.add('frame4').add('end');
+          .to('#backgroundfade,.stars,.test,.legal', 2, {opacity:0, ease:easeOut})
+          .to('#bg2', 1, {opacity:1, ease:easeIn})
+          .to('.test2', 1, {opacity:1, ease:easeIn}) 
+          .to('.cta', 1, {opacity:1, ease:easeIn})
+         timeline.add('frame3').add('end');
