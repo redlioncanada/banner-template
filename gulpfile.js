3bb6db4dcca60beb232d77fed9835e51a15f3c9a
Stephen Poole <stephen.poole@redlioncanada.com>
2016-08-08 13:40:33 -0400

    Adjusted package naming convention

--- gulpfile.js
+++ gulpfile.js
@@ -214,37 +214,61 @@ gulp.task('default', ['save'], function() {
     gulp.watch('app/assets/**/*.*', ['save']);
 });
 
-gulp.task('package', ['validate', 'packageTask']);
-gulp.task('publish', ['validate', 'packageTask']);
-gulp.task('packageTask', function() {
+gulp.task('package', ['packageContinueTask']);
+gulp.task('publish', ['packageContinueTask']);
+
+gulp.task('packageTask', ['validate'], function() {
     var zip = require('gulp-zip');
     var tasks = [];
+
+    var year = new Date().getFullYear()
+    var month = getMonth(new Date().getMonth())
+    var brand = config.brand
+    var version = config.version
+    var name = config.name
+
     for (var i in config.sizes) {
         for (var k in config.text) {
             for (var j in config.clicktags) {
                 var clicktag = config.clicktags[j];
                 var size = config.sizes[i];
                 var language = k;
-                var version = config.version
-                var name = config.packageName.replace('{size}',size).replace('{language}',language).replace('{version}',version)
                 var path = 'build/'+size+'-'+clicktag+'/'+language+'/*';
+
+                var packageName = `${year}_${brand}Brand_RL_Other_${name}_Retail${month}_HTML5_CA_${language.toUpperCase()}_${size}`
 
                 tasks.push(gulp.src(path)
                     .pipe(ignore(['index.fat.html']))
-                    .pipe(zip(name+'.zip'))
+                    .pipe(zip(packageName+'.zip'))
                     .pipe(gulp.dest('build/package/'+clicktag)));
             }
         }
     }
 
+    return mergeStream(tasks);
+});
+
+gulp.task('packageContinueTask', ['packageTask'], function() {
+    var zip = require('gulp-zip');
+    var tasks = [];
+
+    var year = new Date().getFullYear()
+    var month = getMonth(new Date().getMonth())
+    var brand = config.brand
+    var version = config.version
+    var name = config.name
+
     for (var j in config.clicktags) {
-        tasks.push(gulp.src('build/package/'+config.clicktags[j]+'/**/*.zip')
-            .pipe(zip(config.name+'-'+config.clicktags[j]+'.zip'))
+        var clicktag = config.clicktags[j]
+        var name = `${year}_${brand}Brand_RL_Other_${name}_Retail${month}_HTML5_CA_${clicktag}_V${version}`
+
+        tasks.push(gulp.src('build/package/'+clicktag+'/**/*.zip')
+            .pipe(zip(name+'.zip'))
             .pipe(gulp.dest('build/package')));
     }
 
     return mergeStream(tasks);
-});
+})
 
 gulp.task('save', ['cleanSave'], function() {
     if (argv.save && config.name) {
@@ -259,11 +283,15 @@ gulp.task('cleanSave', ['generateHtml'], function() {
     }
 });
 
+gulp.task('cleanPackage', function() {
+    return del(['build/package']);
+});
+
 gulp.task('clean', function() {
     return del(['build']);
 });
 
-gulp.task('validate', function() {
+gulp.task('validate', ['cleanPackage'], function() {
     var tasks = [];
     for (var i in config.sizes) {
         for (var k in config.text) {
@@ -320,3 +348,33 @@ function generateSrcFolders(path,params) {
     return src;
 }
 
+function getMonth(month) {
+    switch(month) {
+        case 0:
+            return "January"
+        case 1:
+            return "February"
+        case 2:
+            return "March"
+        case 3:
+            return "April"
+        case 4:
+            return "May"
+        case 5:
+            return "June"
+        case 6:
+            return "July"
+        case 7:
+            return "August"
+        case 8:
+            return "September"
+        case 9:
+            return "October"
+        case 10:
+            return "November"
+        case 11:
+            return "December"
+        default:
+            return false
+    }
+}
