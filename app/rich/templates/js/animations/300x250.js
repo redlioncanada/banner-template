39e7ac52025e8456a6d5214bcc0057af5294ec35
Stephen Poole <stephen.poole@redlioncanada.com>
2016-08-24 14:30:17 -0400

    Completed all sizes

--- app/rich/templates/js/animations/300x250.js
+++ app/rich/templates/js/animations/300x250.js
@@ -5,8 +5,8 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('.copy1,.copy2', 0, {opacity: 1})
             .to('.masks .m1', 0.8, {width:301,left:0}, 'start')
             .to('.masks .m2', 0.8, {height:196}, 'start').add('frame1-m2')
-            .to('.masks .m3', 0.8, {width:139}, 'frame1-m2').add('frame1-m3')
-            .to('.masks .m4', 0.8, {width:165}, 'frame1-m3-=0.1')
+            .to('.masks .m3', 0.8, {width:175}, 'frame1-m2').add('frame1-m3')
+            .to('.masks .m4', 0.8, {width:127}, 'frame1-m3')
             .to('.copy2', 0.8, {left:0})
             .addDelay(2, 'frame2')
         timeline.add('frame2')
@@ -14,7 +14,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .to('.masks', 0, {opacity:0})
         timeline.add('frame3')
             .to('.background-container', 0, {css:{className:'+=frame3'}})
-            .to('.background-container,.copy3', 0, {top: -13})  //correct for larger copy3 size
+            .to('.background-container', 0, {top: -13})  //correct for larger copy3 size
             .to('.background-container,.logo', 0.8, {opacity:1})
             .to('.copy3', 0, {opacity:1})
             .to('.background3', 0, {opacity:1})
--- app/rich/templates/js/animations/300x600.js
+++ app/rich/templates/js/animations/300x600.js
@@ -11,7 +11,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .addDelay(2, 'frame2')
         timeline.add('frame2')
             .to('.copy2', 0.8, {left:-{width}}, 'frame2')
-            .to('.copy1', 0.8, {top:'-=65'}, 'frame2')
+            .to('.copy1', 0.8, {top:0}, 'frame2')
             .to('.copy3', 0.8, {left:0})
             .addDelay(2, 'frame3')
         timeline.add('frame3')
