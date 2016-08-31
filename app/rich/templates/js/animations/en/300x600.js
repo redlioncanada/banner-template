dc8f8821c19189a7ab0352095de25d4e0c27a9c6
Stephen Poole <stephen.poole@redlioncanada.com>
2016-08-31 11:22:11 -0400

    Completed all FR sizes

--- app/rich/templates/js/animations/en/300x600.js
+++ app/rich/templates/js/animations/en/300x600.js
@@ -1,6 +1,5 @@
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
-var test = test;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1,.copy2,.copy3', 0, {opacity: 1})
@@ -12,7 +11,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .addDelay(2, 'frame2')
         timeline.add('frame2')
             .to('.copy2', 0.8, {left:-{width}}, 'frame2')
-            .to('.copy1', 0.8, {top:'-=46'}, 'frame2')
+            .to('.copy1', 0.8, {top: '-=46'}, 'frame2')
             .to('.copy3', 0.8, {left:0})
             .addDelay(2, 'frame3')
         timeline.add('frame3')
