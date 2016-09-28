82cf9378ff13423f72168b75b0a5cd137f347478
Storm Henry <storm.henry@redlioncanada.com>
2016-09-28 14:23:54 -0400

    Assets optimized and mobile completed

--- app/rich/templates/js/animations/300x50.js
+++ app/rich/templates/js/animations/300x50.js
@@ -2,12 +2,14 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
-            .to('.copy', 1.2, {left:0})
+            .addDelay(0.6)
+            .to('.copy1', 0.6, {opacity:1})
             .addDelay(3, 'frame2')
         timeline.add('frame2')
-            .to('.copy', 0.6, {opacity:0})
-            .to('.cta', 0.6, {opacity:1})
+            .to('.copy1', 0.6, {opacity:0})
+            .to('.copy2', 0.6, {opacity:1})
             .addDelay(2, 'frame3')
         timeline.add('frame3')
+            .to('.copy2', 0.6, {opacity:0})
             .to('.cta', 0.8, {opacity:1})
         timeline.add('frame4')
