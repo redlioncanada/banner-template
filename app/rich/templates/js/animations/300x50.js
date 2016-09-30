4fd14e211599da3d083750602b9687974e4704ad
Storm Henry <storm.henry@redlioncanada.com>
2016-09-30 09:26:52 -0400

    Corrections made to the files and assets sharpened

--- app/rich/templates/js/animations/300x50.js
+++ app/rich/templates/js/animations/300x50.js
@@ -1,15 +1,20 @@
+
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
-            .addDelay(0.6)
-            .to('.copy1', 0.6, {opacity:1})
+            .to('.copy1', 1.2, {left:0})
             .addDelay(3, 'frame2')
         timeline.add('frame2')
-            .to('.copy1', 0.6, {opacity:0})
-            .to('.copy2', 0.6, {opacity:1})
-            .addDelay(2, 'frame3')
+            .to('.copy1', 0.6, {left:-{width}})
+            .to('.copy2', 0.6, {left:0})
+            .addDelay(3, 'frame3')
         timeline.add('frame3')
             .to('.copy2', 0.6, {opacity:0})
-            .to('.cta', 0.8, {opacity:1})
+            .to('.cta', 0.6, {opacity:1})
+            .addDelay(2, 'frame4')
         timeline.add('frame4')
+            .to('.cta', 0.8, {opacity:1})
+timeline.add('frame5')
+
+
