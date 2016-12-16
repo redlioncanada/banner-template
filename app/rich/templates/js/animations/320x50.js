45dee1bb466d4f6232e39a0453d983100e41079f
Storm Henry <storm.henry@redlioncanada.com>
2016-12-16 16:36:08 -0500

    Assets changed, statics added

--- app/rich/templates/js/animations/320x50.js
+++ app/rich/templates/js/animations/320x50.js
@@ -1,3 +1,4 @@
+
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
@@ -6,12 +7,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
             .addDelay(3, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 0.6, {left:-{width}})
-            .to('.copy2', 0.6, {left:0})
-            .addDelay(3, 'frame3')
+            .addDelay(1, 'frame3')
         timeline.add('frame3')
-            .to('.copy2', 0.6, {opacity:0})
-            .to('.cta', 0.6, {opacity:1})
-            .addDelay(2, 'frame4')
-        timeline.add('frame4')
             .to('.cta', 0.8, {opacity:1})
-        timeline.add('frame5')
+        timeline.add('frame4')
--- app/rich/templates/js/animations/en/728x90.js
+++ app/rich/templates/js/animations/en/728x90.js
@@ -3,7 +3,7 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1,.copy2,.copy3,.logo', 0, {opacity: 1})
-            .to('.masks .m1', 0.8, {height:45})
+            .to('.masks .m1', 0.8, {height:56})
             .to('.masks .m2', 0.8, {height:46})
             .to('.masks .m3', 0.8, {top:0}, 'frame1')
             .to('.copy2', 0.8, {left:"+=388"})
@@ -11,7 +11,7 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame2')
             .to('.copy2', 0.8, {left:"-=388"}).add('frame2-backgrounds')
             .to('.copy3', 0.8, {left:"+=418"}, 'frame2-backgrounds')
-            .to('.backgrounds,.copy', 0.65, {left:-29}, 'frame2-backgrounds')
+            .to('.backgrounds,.copy', 0.65, {left:-34}, 'frame2-backgrounds')
             .addDelay(1, 'frame3')
         timeline.add('frame3')
             .to('.cta', 0.8, {opacity:1})
