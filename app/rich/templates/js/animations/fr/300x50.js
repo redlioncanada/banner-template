3b84321d0352663ab735f9bb7bd1001f95573003
Storm Henry <storm.henry@redlioncanada.com>
2017-03-01 11:10:10 -0500

    All french banners completed

--- app/rich/templates/js/animations/fr/300x50.js
+++ app/rich/templates/js/animations/fr/300x50.js
@@ -2,8 +2,8 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
-            .to('.copy-event-1', 1, {left: -170})
-            .to('.copy-event-2', 0.5, {left: -170}, '-=0.3')
+            .to('.copy-event-1', 1, {left: -66})
+            .to('.copy-event-2', 0.5, {left: -66}, '-=0.3')
             .addDelay(2.7, 'frame2')
         timeline.add('frame2')
             .to('.copy-event', 1, {opacity: 0})
@@ -13,6 +13,8 @@ var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame3')
             .to('.copy2', 1, {opacity: 0})
             .to('.copy3', 1, {opacity: 1})
-            .to('.copy4', 1, {opacity: 1})
-            .to('.cta', 0.8, {opacity: 1})
+            .addDelay(2, 'frame4')
         timeline.add('frame4')
+            .to('.copy3', 1, {opacity: 0})
+            .to('.cta', 0.8, {opacity: 1})
+        timeline.add('frame5')
