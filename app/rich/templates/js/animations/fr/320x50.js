9af6e0376ea9a81cd993e7777648903dcef5158c
Storm Henry <storm.henry@redlioncanada.com>
2017-08-28 15:56:29 -0400

    French version created, statics not completed

--- app/rich/templates/js/animations/fr/320x50.js
+++ app/rich/templates/js/animations/fr/320x50.js
@@ -3,10 +3,10 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1', 1, {opacity: 1})
-            .addDelay(2.7, 'frame2')
+            .addDelay(2, 'frame2')
         timeline.add('frame2')
-            .to('.copy1', 1, {opacity: 0})
-            .to('.nameplate', 1, {top: 0})
+            .to('.copy1', 0.5, {opacity: 0})
+            .to('.nameplate', 0.5, {top: 0})
             .addDelay(0.5, 'frame2-copy1')
             .to('.copy2', 1, {opacity: 1}).add('frame2-copy1')
             .to('.masks', 1, {opacity: 0}, '-=1')
