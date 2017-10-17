0c309f99d6b9701084f10a6d620ab5a803ced425
Storm Henry <storm.henry@redlioncanada.com>
2017-10-17 16:55:21 -0400

    did all the banners and fixed all the animations

--- app/rich/templates/js/animations/en/160x600.js
+++ app/rich/templates/js/animations/en/160x600.js
@@ -3,11 +3,11 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
              .to('.copy1', 1.5, {opacity: 1})
-            .addDelay(1, 'frame2')
+            .addDelay(2.5, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 1, {opacity: 0})
             .to('.copy2', 2, {opacity: 1},'-=1')
-            .addDelay(1, 'frame3')
+            .addDelay(2.5, 'frame3')
         timeline.add('frame3')
             .to('.copy2', 1, {opacity: 0})
             .to('.copy3', 2, {opacity: 1},'-=1')
