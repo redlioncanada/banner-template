677820632ec4f225ed706034b04077cf2b59a666
Storm Henry <storm.henry@redlioncanada.com>
2017-10-25 16:37:00 -0400

    added delays to the french SS and LB animations

--- app/rich/templates/js/animations/fr/160x600.js
+++ app/rich/templates/js/animations/fr/160x600.js
@@ -3,7 +3,7 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
              .to('.copy1', 1.5, {opacity: 1})
-            .addDelay(1, 'frame2')
+            .addDelay(2, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 1, {opacity: 0})
             .to('.copy2', 2, {opacity: 1},'-=1')
