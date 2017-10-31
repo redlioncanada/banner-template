574919289639381fa78aac499005628f47d57bff
Storm Henry <storm.henry@redlioncanada.com>
2017-10-31 15:09:11 -0400

    Changed hold on animation to longer LB

--- app/rich/templates/js/animations/fr/728x90.js
+++ app/rich/templates/js/animations/fr/728x90.js
@@ -3,11 +3,11 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1', 1.5, {opacity: 1})
-            .addDelay(2.5, 'frame2')
+            .addDelay(3.5, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 1, {opacity: 0})
             .to('.copy2', 1, {opacity: 1}, '-=1')
-            .addDelay(2.5, 'frame3')
+            .addDelay(3.5, 'frame3')
         timeline.add('frame3')
             .to('.copy2', 1, {opacity: 0})
             .to('.copy3', 1, {opacity: 1}, '-=1')
