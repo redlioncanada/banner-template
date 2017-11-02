3d36748736848bcf34df02ffd65b576455449010
Storm Henry <storm.henry@redlioncanada.com>
2017-11-02 11:55:19 -0400

    French finished

--- app/rich/templates/js/animations/fr/728x90.js
+++ app/rich/templates/js/animations/fr/728x90.js
@@ -3,11 +3,11 @@ var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1', 1.5, {opacity: 1})
-            .addDelay(2, 'frame2')
+            .addDelay(3, 'frame2')
         timeline.add('frame2')
             .to('.copy1', 1, {opacity: 0})
             .to('.copy2', 1, {opacity: 1}, '-=1')
-            .addDelay(2, 'frame3')
+            .addDelay(3, 'frame3')
         timeline.add('frame3')
             .to('.cta', 0.5, {opacity: 1})
         timeline.add('frame4')
