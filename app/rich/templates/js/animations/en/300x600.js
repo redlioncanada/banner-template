3f045474b88d3ad8c8afac20054d7d7905017689
Storm Henry <storm.henry@redlioncanada.com>
2018-04-13 13:59:48 -0400

    Added the new animation... The statics still need to be done though

--- app/rich/templates/js/animations/en/300x600.js
+++ app/rich/templates/js/animations/en/300x600.js
@@ -1,15 +1,19 @@
+
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete});
         timeline.add('frame1')
             .to('.copy1', 1, {opacity: 1})        
-            .addDelay(2.7, 'frame2')
+            .addDelay(2, 'frame2')
         timeline.add('frame2')
-            .to('.background2', 1, {opacity:1})
-            .to('.copy1', 0.5, {opacity: 0},'-=1')
-            .to('.copy2', 0.5, {opacity: 1},'-=1')
-            .addDelay(2, 'frame3')
+            .to('.background2', 1, {opacity: 1})
+            .to('.background1', 0, { opacity: 0 })
+            .addDelay(2.7, 'frame3')
         timeline.add('frame3')
-            .to('.background3', 0.5, {opacity:1})
-            .to('.cta', 1, {opacity: 1},'-=0.5')
+            .to('.wipe', 0.5, { width: 300 })
+            .to('.wipe-line', 0.5, { left: 300 }, '-=0.5')
+            .to('.wipe-line', 0.3, { left: 310 })
+            .addDelay(1, 'frame3')
         timeline.add('frame4')
+            .to('.cta', 1, {opacity: 1})
+        timeline.add('frame5')
