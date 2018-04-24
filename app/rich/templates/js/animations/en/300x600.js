34a96f3486eb1d81dedba106093d47624d2c44bf
Storm Henry <storm.henry@redlioncanada.com>
2018-04-24 14:09:00 -0400

    New animations statics still not done

--- app/rich/templates/js/animations/en/300x600.js
+++ app/rich/templates/js/animations/en/300x600.js
@@ -1,15 +1,15 @@
+
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete}); 
         timeline.add('frame1')
-            .addDelay(2)
+            .addDelay(2.7)
             .to('.copy1', 1, { opacity: 0 })
             .to('.background1', 13, { scale: 1.1 }, '-=3')
 			.to('.copy2', 1, {opacity: 1 }, '-=9')
-			.addDelay(1)
+			.addDelay(2.7)
 			.to('.copy2', 1, {opacity: 0 }, '-=7')
 			.to('.copy3', 1, {opacity: 1 }, '-=5')
-			.addDelay(1)
 		timeline.add('frame2')
 			.to('.copy4, .vertical-bar', 0.5, {opacity: 0}, '-=3')
 			.to('.cta', 0.5, {opacity: 1}, '-=2')
--- app/rich/templates/js/animations/en/320x50.js
+++ app/rich/templates/js/animations/en/320x50.js
@@ -1,16 +1,16 @@
+
 var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete}); 
         timeline.add('frame1')
-            .addDelay(2)
+            .addDelay(2.7)
             .to('.copy1', 1, { opacity: 0 })
             .to('.background1', 13, { scale: 1.1 }, '-=3')
-			.to('.copy2, .copy4', 1, {opacity: 1 }, '-=9')
-			.to('.vertical-bar', 1, {opacity: 0.5 }, '-=9')
-			.addDelay(1)
+			.to('.copy2', 1, {opacity: 1 }, '-=9')
+			.addDelay(2.7)
 			.to('.copy2', 1, {opacity: 0 }, '-=7')
 			.to('.copy3', 1, {opacity: 1 }, '-=5')
 		timeline.add('frame2')
-			.to('.copy4, .vertical-bar', 0.5, {opacity: 0}, '-=3')
+			.to('.copy4, vertical-bar', 0.5, {opacity: 0}, '-=3')
 			.to('.cta', 0.5, {opacity: 1}, '-=2')
 		timeline.add('frame3')
