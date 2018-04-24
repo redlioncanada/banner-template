e97d23846cc0b56fd0d04f3658948fc6661d29f2
Storm Henry <storm.henry@redlioncanada.com>
2018-04-24 13:57:36 -0400

    New animations

--- app/rich/templates/js/animations/en/300x250.js
+++ app/rich/templates/js/animations/en/300x250.js
@@ -2,11 +2,11 @@ var easeIn = Power1.easeIn;
 var easeOut = Power1.easeOut;
 var timeline = new TimelineLite({onComplete: onComplete}); 
         timeline.add('frame1')
-            .addDelay(2)
+            .addDelay(2.7)
             .to('.copy1', 1, { opacity: 0 })
             .to('.background1', 13, { scale: 1.1 }, '-=3')
 			.to('.copy2', 1, {opacity: 1 }, '-=9')
-			.addDelay(1)
+			.addDelay(2.7)
 			.to('.copy2', 1, {opacity: 0 }, '-=7')
 			.to('.copy3', 1, {opacity: 1 }, '-=5')
 		timeline.add('frame2')
